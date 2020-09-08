8d353bdd08412415401dc1d1f5e4d5af658b8eaa
Nicholas Trujillo <nicholasrtrujillo@gmail.com>
2020-09-08 16:59:00 -0700

    assignment 10 complete

---
+++ main.js
@@ -0,0 +1,17 @@
+setInterval(() => {
+  let clock = document.querySelector("#clock");
+
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+
+  if (h > 12) {
+    h = h - 12;
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  clock.innerHTML = h + ":" + m + ":" + s;
+});
